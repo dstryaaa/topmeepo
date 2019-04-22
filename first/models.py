bf0eab43030a90ba0494af31c4338eed9262eb4b
skwadet <skwadet@gmail.com>
2019-04-23 01:42:14 +0300

    20 apr

--- first/models.py
+++ first/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField('title', max_length=50)
     content = models.TextField('content', null=True, blank=True)
     published = models.DateTimeField('published', auto_now_add=True, db_index=True)
+    username = models.ForeignKey('User', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Пост'
--- first/views.py
+++ first/views.py
@@ -8,27 +8,32 @@ from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.views.generic.base import View
+from django.contrib.auth.decorators import login_required
 
-
-def index(request):
-    if not request.user.is_authenticated:
+# Аккаунт пользователя
+@login_required(login_url='../login/')
+# Интересный факт: если сюда не поставить user_id, то выдаст ошибку
+def index(request, user_id):
+    if request.user.id != user_id:
         return HttpResponseRedirect('../login/')
     if request.method == 'POST':
         postform = PostForm(request.POST)
         postform.save()
     postform = PostForm()
-    posts = Post.objects.order_by('-published')
+    posts = Post.objects.all()
     return render(request, 'first/index.html', {'posts': posts, 'postform': postform})
 
 
+# Общий фид
 def feed(request):
     posts = Post.objects.order_by('-published')
     return render(request, 'first/feed.html', {'posts': posts,})
 
 
+# Регистрация
 class Signup(FormView):
     form_class = UserCreationForm
-    success_url = "../first"
+    success_url = "../feed/"
     template_name = "first/signup.html"
 
     def form_valid(self, form):
@@ -36,10 +41,11 @@ class Signup(FormView):
         return super(Signup, self).form_valid(form)
 
 
+# Логинка
 class Login(FormView):
     form_class = AuthenticationForm
     template_name = "first/login.html"
-    success_url = "../first/"
+    success_url = "../feed/"
 
     def form_valid(self, form):
         self.user = form.get_user()
@@ -47,6 +53,7 @@ class Login(FormView):
         return super(Login, self).form_valid(form)
 
 
+# Логаут
 class Logout(View):
     def get(self, request):
         logout(request)
