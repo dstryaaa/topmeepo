de1c8f49f1a286957ab0889ff6069661951c27c6
skwadet <skwadet@gmail.com>
2019-04-25 01:45:45 +0300

    20 apr

--- first/models.py
+++ first/models.py
@@ -1,14 +1,48 @@
 from django.db import models
-
+from django.db.models import Sum
 from django.contrib.auth.models import User
-from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
+
+class LikeDislikeManager(models.Manager):
+    use_for_related_fields = True
+
+    def likes(self):
+        # Забираем queryset с записями больше 0
+        return self.get_queryset().filter(vote__gt=0)
+
+    def dislikes(self):
+        # Забираем queryset с записями меньше 0
+        return self.get_queryset().filter(vote__lt=0)
+
+    def sum_rating(self):
+        # Забираем суммарный рейтинг
+        return self.get_queryset().aggregate(Sum('vote')).get('vote__sum') or 0
+
+
+class LikeDislike(models.Model):
+    # Лайки и дислайки плюсуются и минусуются на один
+    like = 1
+    dislike = -1
+    # Какие мнения есть
+    votes = ((dislike, 'Не нравится'), (like, 'Нравится'))
+    vote = models.SmallIntegerField(verbose_name=("Мнение"), choices=votes)
+    # Какой юзер делает
+    user = models.ForeignKey(User, verbose_name=("Юзер"), on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    # Поле для связи
+    content_object = GenericForeignKey()
+    objects = LikeDislikeManager()
 
 
 class Post(models.Model):
-    #title = models.CharField('title', max_length=50)
+    title = models.CharField('title', max_length=50)
     content = models.TextField('content', null=True, blank=True)
     published = models.DateTimeField('published', auto_now_add=True, db_index=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    votes = GenericRelation(LikeDislike, related_query_name='posts')
 
     class Meta:
         verbose_name = 'Пост'
--- first/views.py
+++ first/views.py
@@ -1,14 +1,18 @@
 #from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse
+import json
 #from django.template import loader
 from django.shortcuts import render
 from .forms import PostForm, BioForm
-from .models import Post, Bio
+from .models import Post, Bio, LikeDislike
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.views.generic.base import View
 from django.contrib import admin
+from django.views import View
+from django.contrib.contenttypes.models import ContentType
+
 
 # Аккаунт пользователя
 # Интересный факт: если сюда не поставить user_id, то выдаст ошибку
@@ -36,6 +40,44 @@ def index(request, user_id):
     posts = Post.objects.filter(author=request.user)
     return render(request, 'first/index.html', {'posts': posts, 'postform': postform, 'bios': bios, 'bioform': bioform}, )
 
+
+class Vote(View):
+    model = None  # Модель данных - Статьи или Комментарии, в будущем
+    vote_type = None  # Тип комментария Like/Dislike
+
+    def post(self, request, pk):
+        # Берем объект
+        obj = self.model.objects.get(pk=pk)
+        # GenericForeignKey не поддерживает метод get_or_create
+        try:
+            # Берем инфу об объекте
+            likedislike = LikeDislike.objects.get(content_type=ContentType.objects.get_for_model(obj),
+                                                  object_id=obj.id,
+                                                  user=request.user)
+            # .воут получает тип лайка, сохраняет его, смотри урлы
+            if likedislike.vote is not self.vote_type:
+                likedislike.vote = self.vote_type
+                likedislike.save(update_fields=['vote'])
+                result = True
+            else:
+                # лайк уже доставлен
+                likedislike.delete()
+                result = False
+        except LikeDislike.DoesNotExist:
+            # ставим в первый раз
+            obj.votes.create(user=request.user, vote=self.vote_type)
+            result = True
+
+        return HttpResponse(
+            json.dumps({
+                "result": result,
+                "like_count": obj.votes.likes().count(),
+                "dislike_count": obj.votes.dislikes().count(),
+                "sum_rating": obj.votes.sum_rating()
+            }),
+            content_type="application/json"
+        )
+# ЗАВТРА ДОПИШУ АЯКС ЗАПРОСЫ
 
 def kek(request):
     return HttpResponse('НЕПРАВИЛЬНО')
